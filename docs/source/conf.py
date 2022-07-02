514ad8fcf1d8f479e5a6a3b48ed49c007d0dce69
兮言 <914814442@qq.com>
2022-07-03 06:41:46 +0800

    docs/

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,10 +12,10 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../../CCDeep'))
-sys.path.insert(0, os.path.abspath('../..'))
-sys.path.insert(0, os.path.abspath('..'))
 sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(0, os.path.abspath('..'))
+sys.path.insert(0, os.path.abspath('../..'))
+sys.path.insert(0, os.path.abspath('../../CCDeep'))
 
 
 # -- Project information -----------------------------------------------------
@@ -33,7 +33,14 @@ release = '1.2'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc',
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',
+    'sphinx.ext.doctest',
+    'sphinx.ext.intersphinx',
+    'sphinx.ext.todo',
+    'sphinx.ext.coverage',
+    'sphinx.ext.mathjax',
 
 ]
 
@@ -50,7 +57,7 @@ language = 'zh_CN'
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = []
+exclude_patterns = ['../../CCDeep/main.py']
 
 
 # -- Options for HTML output -------------------------------------------------
@@ -58,7 +65,6 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-# html_theme = 'alabaster'
 html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
